e453382151696c466ae806ab1087ff6643aea44e
Hugo Guerrier <hugogguerrier@gmail.com>
2021-10-11 11:22:56 +0200

    Initial commit

---
+++ src/test/java/com/lambda/cvlibrary/CvlibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lambda.cvlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvlibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
